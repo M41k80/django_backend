b874644893b460edfccd684b39228109559ed769
Magdiel Mora <m41k80@icloud.com>
2025-04-08 16:23:26 -0500

    done

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -14,6 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+config = Config()
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ['.onrender.com']
