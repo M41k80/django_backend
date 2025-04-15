28b08edeb64dbdcd7dc06b6c1e771f8a212cee86
Magdiel Mora <m41k80@icloud.com>
2025-04-14 21:08:14 -0500

    first commit3

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['.onrender.com', 'http://localhost:3000']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
